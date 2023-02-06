2df3bd62b30732c71ccb2238e17201c9c9cdfc04
mathis Krier <mathis.krier@hotmail.fr>
2023-02-06 14:17:25 +0100

    toggle function working

--- app/controllers/ski_spots_controller.rb
+++ app/controllers/ski_spots_controller.rb
@@ -3,6 +3,5 @@ class SkiSpotsController < ApplicationController
     @spots = SkiSpot.all
     #@regions = @spots.map(:region).uniq
     @regions = @spots.map{|spot| spot.region}.uniq
-
   end
 end
--- app/javascript/controllers/resort_list_controller.js
+++ app/javascript/controllers/resort_list_controller.js
@@ -3,12 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="resort-list"
 
 export default class extends Controller {
+  static targets= ["spotList"]
   connect() {
     console.log("hello from resort_list_controller.js")
   }
   //data-action=click->resort-list#show
+  //data-resort-list-target="link"
   show() {
     console.log("connected with button!")
-
+    this.spotListTarget.classList.toggle("hidden")
   }
 }
